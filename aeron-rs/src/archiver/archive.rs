ca916861c07a4ba6583be5768b6daac20233b4dd
rafalpiotrowski <rafalpiotrowski@users.noreply.github.com>
2024-09-20 06:53:53 +0200

    wip

--- aeron-rs/src/archiver/archive.rs
+++ aeron-rs/src/archiver/archive.rs
@@ -1,3 +1,15 @@
-pub struct Archive {}
+use crate::aeron::Aeron;
+
+pub struct Archive {
+	aeron: Option<Aeron>,
+}
+
+impl Archive {
+	pub fn new() -> Self {
+		Self {
+			aeron: None
+		}
+	}
+}
 
 pub struct Context {}
